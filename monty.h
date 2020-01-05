2c2fd99d5e32de54407b1d6d0639f434a6988444
lh1008 <paintluis@gmail.com>
2020-01-05 15:11:21 -0500

    Nop function no initialized in monty.h

--- monty.h
+++ monty.h
@@ -62,5 +62,6 @@ void op_pint(stack_t **stack, unsigned int line_number);
 void op_pop(stack_t **stack, unsigned int line_number);
 void op_swap(stack_t **stack, unsigned int line_number);
 void op_add(stack_t **stack, unsigned int line_number);
+void op_nop(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
