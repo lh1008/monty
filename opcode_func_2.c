b23c5894060bd8c979e16add0f84c94eb1bfe237
lh1008 <paintluis@gmail.com>
2020-01-05 16:30:15 -0500

    Sub function

--- opcode_func_2.c
+++ opcode_func_2.c
@@ -33,3 +33,26 @@ void op_nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
+
+#include "monty.h"
+
+/**
+ * op_sub - Entry point
+ * Desc: op_sub function
+ * @stack: pointer to pointer to stack_t
+ * @line_number: unsigned int type
+ * Return: Function that adds the top two elements of the stack.
+ **/
+void op_sub(stack_t **stack, unsigned int line_number)
+{
+	if (*stack != NULL && (*stack)->next != NULL)
+	{
+		(*stack)->next->n = (*stack)->next->n - (*stack)->n;
+		op_pop(stack, line_number);
+	}
+	else
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
